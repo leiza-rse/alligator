b5803d769016c00d62bcc860bdae63c6309043ac
Florian Thiery <web@florian-thiery.de>
2018-07-09 09:23:02 +0200

    mod root json

--- src/main/java/de/rgzm/alligator/rest/RootResource.java
+++ src/main/java/de/rgzm/alligator/rest/RootResource.java
@@ -2,8 +2,10 @@ package de.rgzm.alligator.rest;
 
 import de.rgzm.alligator.config.POM;
 import de.rgzm.alligator.log.Logging;
+import de.rgzm.alligator.restconfig.ResponseGZIP;
 import java.io.IOException;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -14,9 +16,9 @@ public class RootResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON + ";charset=UTF-8")
-    public Response getInfo() throws IOException {
+    public Response getInfo(@HeaderParam("Accept-Encoding") String acceptEncoding, @HeaderParam("Accept") String acceptHeader) throws IOException {
         try {
-            return Response.ok(POM.getInfo()).header("Content-Type", "application/json;charset=UTF-8").build();
+            return ResponseGZIP.setResponse(acceptEncoding, POM.getInfo().toString());
         } catch (Exception e) {
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(Logging.getMessageJSON(e, "de.rgzm.alligator.rest.RootResource"))
                     .header("Content-Type", "application/json;charset=UTF-8").build();
