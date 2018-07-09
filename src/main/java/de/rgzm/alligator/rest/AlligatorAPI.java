72354247d6ea33e0a4708555e038530dc42603bd
Florian Thiery <web@florian-thiery.de>
2018-07-09 14:47:40 +0200

    rdf as text

--- src/main/java/de/rgzm/alligator/rest/AlligatorAPI.java
+++ src/main/java/de/rgzm/alligator/rest/AlligatorAPI.java
@@ -4,6 +4,7 @@ import de.rgzm.alligator.config.POM;
 import de.rgzm.alligator.functions.Alligator;
 import de.rgzm.alligator.functions.MatrixAllen;
 import de.rgzm.alligator.functions.MatrixDist;
+import de.rgzm.alligator.functions.RDFEvents;
 import de.rgzm.alligator.functions.Timeline;
 import de.rgzm.alligator.log.Logging;
 import de.rgzm.alligator.restconfig.ResponseGZIP;
@@ -31,14 +32,14 @@ public class AlligatorAPI {
                     .header("Content-Type", "application/json;charset=UTF-8").build();
         }
     }
-    
+
     @POST
     @Path("/matrixallen")
     @Consumes(MediaType.TEXT_PLAIN)
     @Produces(MediaType.APPLICATION_JSON + ";charset=UTF-8")
     public Response loadCAgetMATRIXALLEN(@HeaderParam("Accept-Encoding") String acceptEncoding, @HeaderParam("Accept") String acceptHeader, String tsv) throws IOException {
         try {
-            
+
             // write timeline json
             Alligator alligator = new Alligator();
             alligator = alligator.calculate(tsv);
@@ -50,14 +51,14 @@ public class AlligatorAPI {
                     .header("Content-Type", "application/json;charset=UTF-8").build();
         }
     }
-    
+
     @POST
     @Path("/matrixdist")
     @Consumes(MediaType.TEXT_PLAIN)
     @Produces(MediaType.APPLICATION_JSON + ";charset=UTF-8")
     public Response loadCAgetMATRIXDIST(@HeaderParam("Accept-Encoding") String acceptEncoding, @HeaderParam("Accept") String acceptHeader, String tsv) throws IOException {
         try {
-            
+
             // write timeline json
             Alligator alligator = new Alligator();
             alligator = alligator.calculate(tsv);
@@ -69,20 +70,40 @@ public class AlligatorAPI {
                     .header("Content-Type", "application/json;charset=UTF-8").build();
         }
     }
-    
+
     @POST
     @Path("/timeline")
     @Consumes(MediaType.TEXT_PLAIN)
     @Produces(MediaType.APPLICATION_JSON + ";charset=UTF-8")
     public Response loadCAgetTIMELINEJSON(@HeaderParam("Accept-Encoding") String acceptEncoding, @HeaderParam("Accept") String acceptHeader, String tsv) throws IOException {
         try {
-            
+
             // write timeline json
             Alligator alligator = new Alligator();
             alligator = alligator.calculate(tsv);
             JSONArray timelineJSON = Timeline.writeTimelineJSON(alligator);
             // output
             return ResponseGZIP.setResponse(acceptEncoding, timelineJSON.toString());
+        } catch (Exception e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(Logging.getMessageJSON(e, "de.rgzm.alligator.rest.AlligatorAPI"))
+                    .header("Content-Type", "application/json;charset=UTF-8").build();
+        }
+    }
+
+    @POST
+    @Path("/turtle")
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN + ";charset=UTF-8")
+    public Response loadCAgetRDFFILE(@HeaderParam("Accept-Encoding") String acceptEncoding, @HeaderParam("Accept") String acceptHeader, String tsv) throws IOException {
+        try {
+
+            // write timeline json
+            Alligator alligator = new Alligator();
+            alligator = alligator.calculate(tsv);
+            String rdf = RDFEvents.writeRDFasText(alligator);
+            //rdf = rdf.replace("\r\n", "<br>");
+            // output
+            return Response.ok(rdf).header("Content-Type", "text/plain;charset=UTF-8").build();
         } catch (Exception e) {
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(Logging.getMessageJSON(e, "de.rgzm.alligator.rest.AlligatorAPI"))
                     .header("Content-Type", "application/json;charset=UTF-8").build();
